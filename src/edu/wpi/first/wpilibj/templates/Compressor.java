e4089ca4d803160b3ee7de4ddea0ac6637a7744a
Random178Member <team@farmingtonrobotics.org>
2014-02-17 16:20:21 -0500

    Added SmatDashboard outputs for analogPSI readings.

--- src/edu/wpi/first/wpilibj/templates/Compressor.java
+++ src/edu/wpi/first/wpilibj/templates/Compressor.java
@@ -6,8 +6,10 @@
 
 package edu.wpi.first.wpilibj.templates;
 
+import edu.wpi.first.wpilibj.AnalogChannel;
 import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
@@ -16,9 +18,13 @@ import edu.wpi.first.wpilibj.DigitalInput;
 public class Compressor implements Component{
     Relay compressor = new Relay(4, Relay.Direction.kForward);
     DigitalInput cutoff = new DigitalInput(6);
+	AnalogChannel analogPressure = new AnalogChannel(3);
+	double pressure;
 
     public void tickTeleop() {
         compressor.set(cutoff.get()?Relay.Value.kOff:Relay.Value.kForward);
+		SmartDashboard.putNumber("Pressure", pressure);
+		SmartDashboard.putBoolean("PSI to Shoot?", pressure >= 50);
     }
     public void tickAuto() {
         compressor.set(cutoff.get()?Relay.Value.kOff:Relay.Value.kForward);
