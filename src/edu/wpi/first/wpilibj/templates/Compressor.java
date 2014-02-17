62c599db25d4e26d99796cbc8f033e37267432f2
Random178Member <team@farmingtonrobotics.org>
2014-02-17 18:03:19 -0500

    Fixed the coefficient for pressure

--- src/edu/wpi/first/wpilibj/templates/Compressor.java
+++ src/edu/wpi/first/wpilibj/templates/Compressor.java
@@ -23,7 +23,7 @@ public class Compressor implements Component{
 
     public void tickTeleop() {
         compressor.set(cutoff.get()?Relay.Value.kOff:Relay.Value.kForward);
-		pressure = analogPressure.getVoltage() * 106;
+		pressure = (analogPressure.getVoltage() - .854) * 40.9276;
 		SmartDashboard.putNumber("Pressure", pressure);
 		SmartDashboard.putBoolean("PSI to Shoot?", pressure >= 50);
     }
